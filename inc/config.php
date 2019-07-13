d752d04b04996b4a51b2476fb4e2c117a45baa36
Theron Adrianne Bueno <prodev.Theron@gmail.com>
2019-07-13 22:51:45 +0800

    Fixed Stylesheet in config.php
    Forgot to change the location

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,7 @@
 <?php
     //If there is no constant defined '__CONFIG__', do not proceed loading the page.
     if(!defined('__CONFIG__')){
-        exit('<link rel="stylesheet" href="styles.css"><h1 class="text-center">Access Denied</h1>');
+        exit('<link rel="stylesheet" href="../styles.css"><h1 class="text-center">Access Denied</h1>');
+        
     }
 ?>
